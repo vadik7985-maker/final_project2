bb7de89ecbf6e7bc28a08eec9080708c249d8c40
Софья Жданова <sonyazhdanovaaa@gmail.com>
2026-05-04 01:31:47 +0300

    добавлена 1 модель категория

--- prediction_site/predictions/models.py
+++ prediction_site/predictions/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50, verbose_name="Название")
+    description = models.TextField(blank=True, verbose_name="Описание")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
