b8166b9627a6e8102aae4a5e52e8968207c32caf
Софья Жданова <sonyazhdanovaaa@gmail.com>
2026-05-04 16:26:02 +0300

    добавила view в user и туда добавила функцию создания и авторизации пользователя, и добавила в юрл ссылку на регистрацию

--- prediction_site/prediction_site/urls.py
+++ prediction_site/prediction_site/urls.py
@@ -1,10 +1,13 @@
 from django.urls import include
 from django.contrib import admin
 from django.urls import path
+from users.views import register
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('auth/', include('django.contrib.auth.urls')),
+    path('auth/register/', register, name='register'),
+
     path('', include('predictions.urls')),
 ]
--- prediction_site/users/views.py
+++ prediction_site/users/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import CustomUserCreationForm
 
-# Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('/')
+    else:
+        form = CustomUserCreationForm()
+
+    return render(request, 'registration/register.html', {'form': form})
