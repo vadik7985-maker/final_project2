df3b4dc82e6850a2f8ed3f9eb5620267abca3ced
Софья Жданова <sonyazhdanovaaa@gmail.com>
2026-05-04 18:27:18 +0300

    добавила вью в redictions

--- prediction_site/predictions/views.py
+++ prediction_site/predictions/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from .models import Category, FortuneCookie
 
 
 def home(request):
-    return render(request, 'base.html')
+    categories = Category.objects.all()
+    cookies_count = FortuneCookie.objects.count()
+    categories_count = Category.objects.count()
+
+    context = {
+        'categories': categories,
+        'cookies_count': cookies_count,
+        'categories_count': categories_count,
+    }
+
+    return render(request, 'predictions/home.html', context)
