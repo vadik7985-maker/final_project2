d4c7d206af1b7fbf145b57c849a4aa85a6f6e8fe
Vadim Matveev <vadik7985@gmail.com>
2026-05-04 21:06:54 +0300

    Прописал маршрут для views.get_prediction

--- prediction_site/predictions/urls.py
+++ prediction_site/predictions/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),           # главная страница
-    # остальные пути позже
+    path('', views.home, name='home'),          
+    path('get/', views.get_prediction, name='get_prediction'),
 ]
