d6ba4016234f697d4087906193e6d98356ff2478
Софья Жданова <sonyazhdanovaaa@gmail.com>
2026-05-05 10:55:27 +0300

    добавила ссылки на избранные печеньки

--- prediction_site/predictions/urls.py
+++ prediction_site/predictions/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path('get/', views.get_prediction, name='get_prediction'),
     path('result/<int:prediction_id>/', views.prediction_result, name='prediction_result'),
     path('my-predictions/', views.my_predictions, name='my_predictions'),
+
+    path('favorites/', views.favorites, name='favorites'),
+    path('favorites/add/<int:cookie_id>/', views.add_to_favorites, name='add_to_favorites'),
+    path('favorites/remove/<int:cookie_id>/', views.remove_from_favorites, name='remove_from_favorites'),
 ]
