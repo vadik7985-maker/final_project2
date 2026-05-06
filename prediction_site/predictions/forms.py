db60d506768c68d54456648aa155aad7cf977dcb
Софья Жданова <sonyazhdanovaaa@gmail.com>
2026-05-06 12:08:54 +0300

    валид на возраст регистрации

--- prediction_site/predictions/forms.py
+++ prediction_site/predictions/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import UserProfile
+from datetime import date
 
 class UserProfileForm(forms.ModelForm):
     class Meta: #внутр класс с настройками
@@ -9,3 +10,28 @@ class UserProfileForm(forms.ModelForm):
             'birth_date': forms.DateInput(attrs={'type': 'date'}),
             'bio': forms.Textarea(attrs={'rows': 3}),
         }
+
+    def clean_birth_date(self):
+        birth_date = self.cleaned_data.get('birth_date')
+
+        if birth_date:
+            today = date.today()
+            age = today.year - birth_date.year
+
+            if today.month < birth_date.month or (
+                    today.month == birth_date.month and today.day < birth_date.day
+            ):
+                age -= 1
+
+            if age < 13:
+                raise forms.ValidationError(
+                    'Минимальный возраст для регистрации — 13 лет. '
+                    f'Вам {age} лет, извините!'
+                )
+
+            if age > 120:
+                raise forms.ValidationError(
+                    'Проверьте дату рождения. Возраст не может быть больше 120 лет.'
+                )
+
+        return birth_date
